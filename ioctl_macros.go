8f05603a53e540ae4e4348786aea52efea6991d8
Adam Olech <nddr89@gmail.com>
2022-04-14 23:37:46 +0000

    ioctl_macros.go: fix function signatures

--- ioctl_macros.go
+++ ioctl_macros.go
@@ -2,25 +2,25 @@
 
 package v4l2
 
-func ioctl_ioc(dir int, typ int, nr int, size int) int {
+func ioctl_ioc(dir uint, typ uintptr, nr uint, size uintptr) uint {
 	return (dir << IOC_DIRSHIFT) |
-	(typ << IOC_TYPESHIFT) |
-	(nr << IOC_NRSHIFT) |
-	(size << IOC_SIZESHIFT)
+		(uint(typ) << IOC_TYPESHIFT) |
+		(nr << IOC_NRSHIFT) |
+		(uint(size) << IOC_SIZESHIFT)
 }
 
-func ioctl_io(typ int, nr int) int {
+func ioctl_io(typ uintptr, nr uint, size uintptr) uint {
 	return ioctl_ioc(IOC_NONE, typ, nr, 0)
 }
 
-func ioctl_ior(typ int, nr int, size int) int {
+func ioctl_ior(typ uintptr, nr uint, size uintptr) uint {
 	return ioctl_ioc(IOC_READ, typ, nr, size)
 }
 
-func ioctl_iow(typ int, nr int, size int) int {
+func ioctl_iow(typ uintptr, nr uint, size uintptr) uint {
 	return ioctl_ioc(IOC_WRITE, typ, nr, size)
 }
 
-func ioctl_iowr(typ int, nr int, size int) int {
-	return ioctl_ioc(IOC_WRITE|IOC_READ, typ, nr, size)
+func ioctl_iowr(typ uintptr, nr uint, size uintptr) uint {
+	return ioctl_ioc(IOC_READ|IOC_WRITE, typ, nr, size)
 }
